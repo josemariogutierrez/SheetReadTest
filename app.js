d5386b1f013768f61b6db966f0c1ad1085a87088
Jose Mario Gutierrez <jogutierrez@H7689.ad.hugeinc.com>
2018-04-13 15:26:44 -0500

    Included logic to get sheet data

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-let spriteSheetId = '';
+const http = require('http');
+const fs = require('fs');
 
-const url = 'https://docs.google.com/spreadsheets/d/1Z6fgUiz2zeTmte01i5ZwroQNCkpjysIP5gkeTFlb_CM'
+var server = http.createServer(
+    function(req,res) {
+  
+        if (req.url === '/home' || req.url === '/') {
+            res.writeHead(200, {'Content-type' : 'text/html'});
+            fs.createReadStream(__dirname + '/index.html','utf8').pipe(res);
+        
+        } 
+    }
+ );
 
-
-fetch(url)
-.then((resp) => resp.json())
-.then(function(data) {
-    console.log(data);
-})
-.catch(function(error) {
-console.log(error);
-});   
+ server.listen(3000,'127.0.0.1');
